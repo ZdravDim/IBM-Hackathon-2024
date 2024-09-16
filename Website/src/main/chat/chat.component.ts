732190871e22a23a0f2f285b3781cb0c2abe23a0
Dimitrije <dimitrijezdravkovic20@gmail.com>
2024-09-16 17:00:55 +0200

    add focus

--- Website/src/main/chat/chat.component.ts
+++ Website/src/main/chat/chat.component.ts
@@ -12,12 +12,21 @@ export class ChatComponent {
 
   messages: any = [];
 
+  loseFocus() {
+    (document.getElementById('message-input') as HTMLInputElement).blur();
+  }
+
   send() {
+    if (this.messages.length > 0 && this.messages[this.messages.length - 1].userMessage === true) {
+      return;
+    }
     const messageText = (document.getElementById('message-input') as HTMLInputElement).value;
     if (messageText) {
       this.messages.push({ text: messageText, userMessage: true });
+      // call api instead of next line and await response...
       this.messages.push({ text: 'I am a bot', userMessage: false });
       (document.getElementById('message-input') as HTMLInputElement).value = '';
+      (document.getElementById('message-input') as HTMLInputElement).focus();
     }
   }
 }
