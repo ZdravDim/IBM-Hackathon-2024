1e0e2f83b4044f4025b1ff1acf4272f622e45262
Dimitrije <dimitrijezdravkovic20@gmail.com>
2024-09-16 16:46:06 +0200

    fix scroll bar

--- Website/src/main/chat/chat.component.ts
+++ Website/src/main/chat/chat.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
 
+  messages: any = [];
+
+  send() {
+    const messageText = (document.getElementById('message-input') as HTMLInputElement).value;
+    if (messageText) {
+      this.messages.push({ text: messageText, userMessage: true });
+      this.messages.push({ text: 'I am a bot', userMessage: false });
+      (document.getElementById('message-input') as HTMLInputElement).value = '';
+    }
+  }
 }
