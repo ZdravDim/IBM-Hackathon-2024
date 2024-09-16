c4a0ec3c083ba93331c7779eb62553cadc7352c1
Dimitrije <dimitrijezdravkovic20@gmail.com>
2024-09-16 16:08:30 +0200

    chat ui

--- Website/src/app/app.component.ts
+++ Website/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "../home/home.component";
-import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, FooterComponent],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
