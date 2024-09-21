9ad7407ad1f2009ff4874e6d02a3e4b66eb4c51f
Dimitrije <dimitrijezdravkovic20@gmail.com>
2024-09-21 19:52:40 +0200

    deployment

--- Python/main.py
+++ Python/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= ["http://localhost:4200"],
+    allow_origins= ["https://zdravdim.github.io/IBM-Hackathon-2024"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
